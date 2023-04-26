79919641f2e65f6a3392241e3ce491d1b94398bd
Class01 <Class01@DESKTOP-1KRANK2>
2023-04-26 17:48:04 +0900

    정올 510번 문제입니다._변수선언, 출력 한줄로

--- Jungol/src/q510/Main.java
+++ Jungol/src/q510/Main.java
@@ -3,11 +3,15 @@ package q510;
 public class Main {
 
 	public static void main(String[] args) {
-		int a = -1;
-		int b = 100;
-
-		System.out.println(a);
-		System.out.println(b);
+//		int a = -1;
+//		int b = 100;
+//
+//		System.out.println(a);
+//		System.out.println(b);
+//		
+		
+		int n1 = -1, n2 = 100;
+		System.out.println(n1 + "\n" + n2);
 		
 // commit message 수정
 		
