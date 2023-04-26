9a977a0ee8090cb3ac255c7839f8aa9e617e17d4
Class01 <Class01@DESKTOP-1KRANK2>
2023-04-26 17:11:11 +0900

    정올 511번 문제입니다.

---
+++ Jungol/src/q511/Main.java
@@ -0,0 +1,49 @@
+package q511;
+
+public class Main {
+
+	public static void main(String[] args) {
+//		int a = 55;
+//		int b = 10;
+//		
+//		int c = 2008;
+//		int d = 1999;
+//		
+//		int result1 = a-b;
+//		int result2 = c-d;
+//		
+//		System.out.println(a + " - " + b + " = " + result1);
+//		System.out.println(c + " - " + d + " = " + result2);
+		
+		
+		
+		int a = 45;
+		int b = 9;
+		
+		System.out.println("55 - 10 = " + a);
+		System.out.println("2008 - 1999 = " + b);
+		
+		
+		
+//		String a = "55 - 10 = ";
+//		String b = "2008 - 1999 = ";
+//		
+//		int x = 45;
+//		int y = 9;
+//		
+//		System.out.println(a + x);
+//		System.out.println(b + y);
+		
+		
+		
+		
+//		String a = "55 - 10 = 45";
+//		String b = "2008 - 1999 = 9";
+//		
+//		System.out.println(a);
+//		System.out.println(b);	
+//		
+
+	}
+
+}
