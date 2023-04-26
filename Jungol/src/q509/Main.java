ba8457619953b9d8d1a0ed8e31d9195cf6622158
Class01 <Class01@DESKTOP-1KRANK2>
2023-04-26 14:52:00 +0900

    정올 509번 문제입니다.

---
+++ Jungol/src/q509/Main.java
@@ -0,0 +1,21 @@
+package q509;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+//		int x = -100;
+//		System.out.println(x);
+//		-100을 출력하기 위해 위와 같이 작성하거나 아래와 같이 작성하여도 된다.		
+		
+		
+		
+//		int a;
+		
+		int a = 0;
+		a= -100;
+			
+		System.out.println(a);
+	}
+
+}
