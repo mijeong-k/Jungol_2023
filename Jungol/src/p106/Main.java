550c2d8a23412082d884466a51ab99eed8bcb4f8
Class01 <Class01@DESKTOP-1KRANK2>
2023-04-27 18:11:31 +0900

    정올 106번 문제

---
+++ Jungol/src/p106/Main.java
@@ -0,0 +1,12 @@
+package p106;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int num1 = 10, num2 = 20, num3= 30;
+		
+		System.out.printf("%d + %d = %d", num1, num2, num3);
+
+	}
+
+}
