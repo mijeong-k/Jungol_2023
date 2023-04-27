7336abdf00b7c8e5d35196c752329d88f756a810
Class01 <Class01@DESKTOP-1KRANK2>
2023-04-27 11:45:30 +0900

    정올 508번 문제

---
+++ Jungol/src/q508/Main.java
@@ -0,0 +1,32 @@
+package q508;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String s1 = "item", s2="count", s3="price";	
+		System.out.printf("%10s%10s%10s\n", s1 , s2, s3);
+		
+		s1 = "pen";
+		s2 = "20";
+		s3 = "100";
+		System.out.printf("%10s%10s%10s\n", s1 , s2, s3);
+		
+		s1 = "note";
+		s2 = "5";
+		s3 = "95";
+		System.out.printf("%10s%10s%10s\n", s1 , s2, s3);
+
+		s1 = "eraser";
+		s2 = "110";
+		s3 = "97";
+		System.out.printf("%10s%10s%10s", s1 , s2, s3);
+		
+		
+//		System.out.printf("%10s%10s%10s\n", "item" , "count", "price");
+//		System.out.printf("%10s%10d%10d\n", "pen" , 20, 100);
+//		System.out.printf("%10s%10s%10s", "eraser" , "110", "97");
+		
+		
+	}
+
+}
