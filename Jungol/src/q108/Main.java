57d3ea17da84c2d0ad8b14de6ef89480358735c2
Class01 <Class01@DESKTOP-1KRANK2>
2023-04-27 18:11:13 +0900

    정올 108번 문제

---
+++ Jungol/src/q108/Main.java
@@ -0,0 +1,15 @@
+package q108;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int num1 = 50;
+		double num2 = 100.12;
+		
+		System.out.printf("%.2f * %d = %.0f\n", num2, num1 , num2*num1);
+	
+//		System.out.printf("%.2f * %d = %d\n", num2, num1, (int)(num2 * num1));
+
+	}
+
+}
