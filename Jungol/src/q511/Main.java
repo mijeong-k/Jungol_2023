61561416c4b7830971b25137ef0f75d50b7912da
Class01 <Class01@DESKTOP-1KRANK2>
2023-04-27 10:09:56 +0900

    정올 511번 문제_산출순서 이용

--- Jungol/src/q511/Main.java
+++ Jungol/src/q511/Main.java
@@ -24,12 +24,19 @@ public class Main {
 //		System.out.println("2008 - 1999 = " + b);
 		
 		
-		int a = 45;
-		int b = 9;
-		
-		System.out.printf("55 - 10 = %d\n", a);
-		System.out.printf("2008 - 1999 = %d", b);
+//		int a = 45;
+//		int b = 9;
+//		
+//		System.out.printf("55 - 10 = %d\n", a);
+//		System.out.printf("2008 - 1999 = %d", b);
 				
+		
+		int num1 = 55, num2 = 10;
+		System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+		num1 = 2008;
+		num2 = 1999;
+		System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+		
 		
 		
 //		String a = "55 - 10 = ";
