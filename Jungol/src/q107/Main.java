2256959311ff30696e42d8714855bcdd0c22cdaa
Class01 <Class01@DESKTOP-1KRANK2>
2023-04-27 18:11:22 +0900

    정올 107번 문제

---
+++ Jungol/src/q107/Main.java
@@ -0,0 +1,13 @@
+package q107;
+
+public class Main {
+
+	public static void main(String[] args) {
+		double d1= 80.5, d2 = 22.34;	
+		System.out.printf("%10.2f%10.2f%10.2f\n", d1 , d2, d1+d2);
+		
+//		float f1= 80.5f, f2 = 22.34f;
+//		System.out.printf("%10.2f%10.2f%10.2f\n", f1 , f2, f1+f2);
+	}
+
+}
