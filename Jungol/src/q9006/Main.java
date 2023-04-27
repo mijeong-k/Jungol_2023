bac15cc2d21626464b018d9c347993f957b153fa
Class01 <Class01@DESKTOP-1KRANK2>
2023-04-27 17:06:45 +0900

    정올 9006번 문제

---
+++ Jungol/src/q9006/Main.java
@@ -0,0 +1,23 @@
+package q9006;
+
+public class Main {
+
+	public static void main(String[] args) {
+//		System.out.println("수를 출력할 때는 따옴표를 생략합니다.");
+//		System.out.println(123);
+//		System.out.println("6.500000");
+		
+		System.out.printf("%s\n", "수를 출력할 때는 따옴표를 생략합니다.");
+		System.out.printf("%d\n%f\n", 123 , 6.500000);
+//		System.out.printf("%d\n%d", 456 , 6.500000); -- Error
+		
+//		double d1 = 6.5;
+//		double d2 = 1236.5078;
+//		System.out.println(d1);
+//		System.out.printf("%f\n", d1);
+//		System.out.printf("%.3f\n", d1);
+//		System.out.printf("%10.3f\n", d1);
+//		System.out.printf("%10.3f\n", d2);
+	}
+
+}
