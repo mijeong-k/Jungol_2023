9645f237e7d60006e54e9570c8b62cf066a22208
Class01 <Class01@DESKTOP-1KRANK2>
2023-05-02 14:29:11 +0900

    정올 9039번 문제

---
+++ Jungol/src/q9039/Main.java
@@ -0,0 +1,14 @@
+package q9039;
+
+public class Main {
+
+	public static void main(String[] args) {
+		char cha = 'A';
+
+		do {
+			System.out.print(cha);
+			cha++;
+		} while (cha < 91);
+	}
+
+}
