6dd772ac08d382a57b412985c282eaec18e69eea
Class01 <Class01@DESKTOP-1KRANK2>
2023-05-04 17:46:59 +0900

    정올 9021번 문제

---
+++ Jungol/src/q9021/Main.java
@@ -0,0 +1,17 @@
+package q9021;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 10;
+
+		System.out.println("최초값 a = " + a + ", b = " + b + "\n");
+		System.out.println("a++ = " + a + ", ++b = " + ++b);
+		System.out.println("실행후 a = " + ++a + ", b = " + b + "\n");
+		System.out.println("a-- = " + a + ", --b = " + --b);
+		System.out.println("실행후 a = " + --a + ", b = " + b);
+
+	}
+
+}
