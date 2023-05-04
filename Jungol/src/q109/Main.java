f602c6d3e8a7a1e67269f88de0b7fc6badc156ab
Class01 <Class01@DESKTOP-1KRANK2>
2023-05-04 09:31:40 +0900

    정올 109번 문제

---
+++ Jungol/src/q109/Main.java
@@ -0,0 +1,17 @@
+package q109;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n1 = sc.nextInt();
+		int n2 = sc.nextInt();
+		int n3 = sc.nextInt();
+		sc.close();
+
+		System.out.println("sum = " + (n1 + n2 + n3));
+	}
+
+}
