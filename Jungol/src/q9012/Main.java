25d84dcc63bf4ee5c8fce922ca161bf89358b239
Class01 <Class01@DESKTOP-1KRANK2>
2023-05-08 18:07:06 +0900

    정올 9012번 문제

---
+++ Jungol/src/q9012/Main.java
@@ -0,0 +1,14 @@
+package q9012;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int n1 = 5, n2 = 2;
+		double d = 3.140000;
+
+		System.out.printf("원주 = %d * %d * %f = %f\n", n1, n2, d, (n1 * n2 * d));
+		System.out.printf("넓이 = %d * %d * %f = %f\n", n1, n1, d, (n1 * n1 * d));
+
+	}
+
+}
