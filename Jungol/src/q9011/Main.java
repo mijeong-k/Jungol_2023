03309a2f3c7db391a97dff8854f28a43b5ef27ab
Class01 <Class01@DESKTOP-1KRANK2>
2023-05-08 17:54:11 +0900

    정올 9011번 문제

---
+++ Jungol/src/q9011/Main.java
@@ -0,0 +1,13 @@
+package q9011;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int n1 = 10, n2 = 20;
+		System.out.println(n1 + " + " + n2 + " = " + (n1 + n2));
+		n1 = 30;
+		n2 = 40;
+		System.out.println(n1 + " + " + n2 + " = " + (n1 + n2));
+	}
+
+}
