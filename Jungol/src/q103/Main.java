6b6bb2d6ad3853613380e0100ea558e124ec5fe8
Class01 <Class01@DESKTOP-1KRANK2>
2023-05-08 17:35:17 +0900

    정올 103번 문제

---
+++ Jungol/src/q103/Main.java
@@ -0,0 +1,11 @@
+package q103;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("TTTTTTTTTT" + "\n" + "TTTTTTTTTT");
+		System.out.printf("%6s\n%6s\n%6s", "TT", "TT", "TT");
+
+	}
+
+}
