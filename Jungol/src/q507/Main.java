78841277a70104e07ad0b2f599ef29f48a75fd21
Class01 <Class01@DESKTOP-1KRANK2>
2023-05-08 17:05:04 +0900

    정올 507번 문제

---
+++ Jungol/src/q507/Main.java
@@ -0,0 +1,13 @@
+package q507;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int n1 = 5;
+		int n2 = 2;
+		System.out.println(n1 + " Dan");
+		System.out.println(n1 + " * " + n2 + " = " + (n1 * n2));
+
+	}
+
+}
