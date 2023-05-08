70f50dbf1201cd44e09dde41260448b27077466d
Class01 <Class01@DESKTOP-1KRANK2>
2023-05-08 17:42:33 +0900

    정올 9013번 문제

---
+++ Jungol/src/q9013/Main.java
@@ -0,0 +1,12 @@
+package q9013;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("전체 7자리로 맞추고 소수 4자리까지 출력");
+		System.out.printf("x = %7.4f\ny = %7.4f\n\n", 1.2340, 10.3459);
+		System.out.println("소수 2자리까지 출력(반올림)");
+		System.out.printf("x = %.2f\ny = %.2f", 1.2340, 10.3459);
+	}
+
+}
