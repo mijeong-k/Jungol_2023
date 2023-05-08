6e570829cee2660a2365ced125d08b4eab854b76
Class01 <Class01@DESKTOP-1KRANK2>
2023-05-08 16:18:38 +0900

    정올 534번 문제

---
+++ Jungol/src/q534/Main.java
@@ -0,0 +1,32 @@
+package q534;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char cinp = sc.next().charAt(0);
+		sc.close();
+
+		switch (cinp) {
+		case 65:
+			System.out.println("Excellent");
+			break;
+		case 66:
+			System.out.println("Good");
+			break;
+		case 67:
+			System.out.println("Usually");
+			break;
+		case 68:
+			System.out.println("Effort");
+			break;
+		case 70:
+			System.out.println("Failure");
+			break;
+		default:
+			System.out.println("error");
+		}
+	}
+}
