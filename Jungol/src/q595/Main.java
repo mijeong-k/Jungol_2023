75e63fe7fc6a3a81fc65a3f22b90721aa808aa6c
Class01 <Class01@DESKTOP-1KRANK2>
2023-05-10 16:05:58 +0900

    정올 595번 문제

---
+++ Jungol/src/q595/Main.java
@@ -0,0 +1,27 @@
+package q595;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String str = "Hong Gil Dong";
+
+//		System.out.println(str.substring(3,7));
+
+//		for (int i = 3; i <= 6; i++) {
+//			char cha = str.charAt(i);
+//			System.out.print(cha);
+//		}
+
+		for (int i = 3; i < 7; i++) {
+			System.out.print(str.charAt(i));
+		}
+		System.out.println();
+		
+		
+//		char[] carr = {'g',' ','G','i'};
+//		for(int i = 0; i < 4; i++) {
+//			System.out.print(carr[i]);
+//		}
+	}
+}
+
