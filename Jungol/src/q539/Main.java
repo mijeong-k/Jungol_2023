20ff5165bc4293f1fce0baba8582379fa31c39ef
Class01 <Class01@DESKTOP-1KRANK2>
2023-05-10 16:05:27 +0900

    정올 539번 배열 문제풀이 추가

--- Jungol/src/q539/Main.java
+++ Jungol/src/q539/Main.java
@@ -7,20 +7,31 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		int sum = 0;
-		int cnt = 0;
-		while (true) {
-			int inp = sc.nextInt();
-			sum += inp;
-			cnt++;
-			if (inp >= 100) {
+//		int sum = 0;
+//		double cnt = 0;
+//		while (true) {
+//			int inp = sc.nextInt();
+//			sum += inp;
+//			cnt++;
+//			if (inp >= 100) {
+//				break;
+//			}
+//		}
+//		sc.close();
+//		System.out.printf("%d\n%.1f\n", sum, (sum / cnt));
+		
+		
+		int[] arr = new int [10];
+		int arrsum=0;
+		for(int i=0; i<arr.length; i++) {
+			int arrinp = sc.nextInt();
+			arr[i]=arrinp;
+			arrsum+=arr[i];
+			if(arrinp>=100) {
 				break;
 			}
-		}
-		sc.close();
-		System.out.println(sum);
-		System.out.printf("%.1f\n", (double)sum / cnt);
-
+		}sc.close();
+		System.out.println(arrsum/arr.length);
 	}
 
 }
