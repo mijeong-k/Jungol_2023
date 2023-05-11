3f0ae0bdc4c3ee6881d679bf99fea062cea7e78f
Class01 <Class01@DESKTOP-1KRANK2>
2023-05-11 14:39:32 +0900

    정올 534번 문제_if 추가

--- Jungol/src/q534/Main.java
+++ Jungol/src/q534/Main.java
@@ -7,8 +7,10 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		char cinp = sc.next().charAt(0);
+		String str = sc.next();
 		sc.close();
 
+		//1. switch-char 문
 		switch (cinp) {
 		case 65:
 			System.out.println("Excellent");
@@ -28,5 +30,43 @@ public class Main {
 		default:
 			System.out.println("error");
 		}
+
+		//2. if문
+		if (cinp == 'A') {
+			System.out.println("Excellent");
+		} else if (cinp == 'B') {
+			System.out.println("Good");
+		} else if (cinp == 'C') {
+			System.out.println("Usually");
+		} else if (cinp == 'D') {
+			System.out.println("Effort");
+		} else if (cinp == 'F') {
+			System.out.println("Failure");
+		} else {
+			System.out.println("error");
+		}
+		
+		//3. switch-string문
+		switch (str) {
+		case "A":
+			System.out.println("Excellent");
+			break;
+		case "B":
+			System.out.println("Good");
+			break;
+		case "C":
+			System.out.println("Usually");
+			break;
+		case "D":
+			System.out.println("Effort");
+			break;
+		case "F":
+			System.out.println("Failure");
+			break;
+		default:
+			System.out.println("error");
+		}
+		
+		
 	}
 }
