b13d587ff1faa4d6d18b0763a7047c9e97dc7633
Class01 <Class01@DESKTOP-1KRANK2>
2023-05-16 14:44:20 +0900

    정올 596번 문제

---
+++ Jungol/src/q596/Main.java
@@ -0,0 +1,49 @@
+package q596;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		int num = sc.nextInt();
+		sc.close();
+
+		// 1.case
+		for (int i = str.length() - 1, cnt = 0; i >= cnt; i--, cnt++) {
+			System.out.print(str.charAt(i));
+		}
+
+		
+		// 2.case
+//		if (num <= str.length()) {
+//			for (int i = 1; i < num + 1; i++) {
+//				System.out.print(str.charAt(str.length() - i));
+//			}
+//
+//		} else {
+//			for (int k = 1; k < str.length() + 1; k++) {
+//				System.out.print(str.charAt(str.length() - k));
+//			}
+//		}
+
+		
+		// 3.case
+//		for (int i = str.length() - 1; (0 <= i && 0 < num); i--, num--) {
+//			System.out.print(str.charAt(i));
+//		}
+//
+//		
+//		if (num < str.length()) {
+//			for (int i = str.length() - 1; i > (str.length() - 1) - num; i--) {
+//				System.out.print(str.charAt(i));
+//			}
+//		} else {
+//			for (int i = str.length() - 1; i >= 0; i--) {
+//				System.out.print(str.charAt(i));
+//			}
+
+	}
+
+}
